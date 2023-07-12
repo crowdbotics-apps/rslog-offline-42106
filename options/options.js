e4dbebfb13e5409094d006b8f85bb3e92736ce85
Sarah Angelou <sarah.angelou@rocscience.com>
2023-07-12 11:28:48 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "rslog-offline-42106",
   url: "https://rslog-offline-42106.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
