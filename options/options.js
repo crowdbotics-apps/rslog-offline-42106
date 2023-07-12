a9c15277eef0acbd8168047f92e447d165d61629
Sarah Angelou <sarah.angelou@rocscience.com>
2023-07-12 11:41:05 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "rslog-offline-42106",
-  url: "https://rslog-offline-42106.botics.co",
-  api: "https://rslog-offline-42106.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "rslog-offline-42106",
+  "url": "https://rslog-offline-42106.botics.co",
+  "api": "https://rslog-offline-42106.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "addCardDetails"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
