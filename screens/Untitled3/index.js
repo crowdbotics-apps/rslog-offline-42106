dade081799d380cf768d931d4757b9ebbd73c8ed
Sarah Angelou <sarah.angelou@rocscience.com>
2023-07-12 13:58:39 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,3 +1,4 @@
+import { View } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -9,7 +10,7 @@ const Untitled3 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><TextInput style={styles.VbDpDKIt}></TextInput></ScrollView>
+    }}><TextInput style={styles.VbDpDKIt}></TextInput><View style={styles.WzthdJfh}></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -22,6 +23,13 @@ const styles = StyleSheet.create({
     borderColor: "#cccccc",
     width: 150,
     height: 30
+  },
+  WzthdJfh: {
+    height: 456,
+    width: 331,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled3;
